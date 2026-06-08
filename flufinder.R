05a372655d913b35b7d154d4529268611b400c89
Karina Robles <kroblesleyva@ucsd.edu>
2026-06-07 20:19:36 -0700

    functions 1-3

--- flufinder.R
+++ flufinder.R
@@ -1,2 +1,24 @@
+upload_fasta <- function(testfasta.txt) {
+  
+  library(seqinr)
+  
+  #Reading the fasta file
+  read.fasta(testfasta.txt, seqtype = "AA", as.string = TRUE,
+             set.attributes = FALSE)}
+
+
+
+trypsinize <- function(testfasta) {
+  
+  library(stringr)
+  
+  lapply(testfasta, str_split_1, pattern = "(?<=R|K)")
+}
+
+
+
+split_peptides <- function(peptides) {
+  library(stringr)
+  lapply(peptides, str_split, pattern = "")}
 
 
