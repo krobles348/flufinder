dde7f976bf850e4b555848b736436f507f8ec8a5
Karina Robles <kroblesleyva@ucsd.edu>
2026-06-07 23:28:26 -0700

    Fix 2

--- flufinder.R
+++ flufinder.R
@@ -1,66 +1,71 @@
-upload_fasta <- function(testfasta.txt) {
+upload_fasta <- function(testfasta_file) {
   
   library(seqinr)
   
-  #Reading the fasta file
-  read.fasta(testfasta.txt, seqtype = "AA", as.string = TRUE,
-             set.attributes = FALSE)}
-
-
+  read.fasta(testfasta_file,
+             seqtype = "AA",
+             as.string = TRUE,
+             set.attributes = FALSE)
+}
 
 trypsinize <- function(testfasta) {
   
   library(stringr)
   
   lapply(testfasta, str_split_1, pattern = "(?<=R|K)")
-  }
-
-
+}
 
 split_peptides <- function(peptides) {
+  
   library(stringr)
-  lapply(peptides, str_split, pattern = "")}
-
-
-
-litpeptides_to_masses <- function(aa) {
   
-  # Generating a vector of masses for each amino acid
-  aa_masses <- c(A=71.037, R=156.101, N=114.042, D=115.026, C=103.009,
-                 Q=128.058, E=129.042, G=57.021, H=137.058, I=113.084, L=113.084,
-                 K=128.094, M=131.040, F=147.068, P=97.052, S=87.032, T=101.047,
-                 W=186.079, Y=163.063, V=99.068)
+  lapply(peptides, str_split, pattern = "")
+}
+
+splitpeptides_to_masses <- function(aa) {
   
-  # Looping over each list of peptides and mapping amino acids to masses,
+  aa_masses <- c(
+    A=71.037, R=156.101, N=114.042, D=115.026, C=103.009,
+    Q=128.058, E=129.042, G=57.021, H=137.058, I=113.084,
+    L=113.084, K=128.094, M=131.040, F=147.068, P=97.052,
+    S=87.032, T=101.047, W=186.079, Y=163.063, V=99.068
+  )
+  
   peptide_masses <- aa
   
   for(i in 1:length(aa)) {
-    peptide_masses[[i]] <- lapply(aa[[i]],
-                                  function(x) sum(aa_masses[x]))
+    peptide_masses[[i]] <- lapply(
+      aa[[i]],
+      function(x) sum(aa_masses[x])
+    )
   }
   
-  # Unlisting the inner lists to generate a list of vectors of masses for
-
   lapply(peptide_masses, unlist)
-  
 }
 
 
 
+
 count_matching_masses <- function(protein_masses, sample) {
+  
+ 
   df <- as.data.frame(sapply(protein_masses, function (x)
     sum(as.character(sample) %in% as.character(x))))
+  
+  # Adding peptide_counts as the column name of the counts column
   names(df) <- "peptide_counts"
-  return(df)}
-
-
+  
+  return(df)
+}
 
 ggbarplot <- function(peptide_counts_table) {
+  
   library(ggplot2)
+  
   ggplot(peptide_counts_table) +
-    aes(rownames(peptide_counts_table), peptide_counts) +
-    geom_col(fill="blue", width=0.5) +
+    aes(x = rownames(peptide_counts_table),
+        y = peptide_counts) +
+    geom_col(fill = "blue", width = 0.5) +
     theme_bw() +
-    labs(x="Flu Strain", y="Peptide Counts")}
-
-
+    labs(x = "Flu Strain", y = "Peptide Counts")
+}
