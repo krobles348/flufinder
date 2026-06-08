d259a2916bc9e25449e0e8abc2a3f0ee74e4f400
Karina Robles <kroblesleyva@ucsd.edu>
2026-06-07 21:55:14 -0700

    fix1

--- flufinder.R
+++ flufinder.R
@@ -13,7 +13,7 @@ trypsinize <- function(testfasta) {
   library(stringr)
   
   lapply(testfasta, str_split_1, pattern = "(?<=R|K)")
-}
+  }
 
 
 
@@ -23,13 +23,15 @@ split_peptides <- function(peptides) {
 
 
 
-splitpeptides_to_masses <- function(aa) {
+litpeptides_to_masses <- function(aa) {
   
   # Generating a vector of masses for each amino acid
   aa_masses <- c(A=71.037, R=156.101, N=114.042, D=115.026, C=103.009,
                  Q=128.058, E=129.042, G=57.021, H=137.058, I=113.084, L=113.084,
                  K=128.094, M=131.040, F=147.068, P=97.052, S=87.032, T=101.047,
                  W=186.079, Y=163.063, V=99.068)
+  
+  # Looping over each list of peptides and mapping amino acids to masses,
   peptide_masses <- aa
   
   for(i in 1:length(aa)) {
@@ -37,6 +39,8 @@ splitpeptides_to_masses <- function(aa) {
                                   function(x) sum(aa_masses[x]))
   }
   
+  # Unlisting the inner lists to generate a list of vectors of masses for
+
   lapply(peptide_masses, unlist)
   
 }
@@ -58,4 +62,5 @@ ggbarplot <- function(peptide_counts_table) {
     geom_col(fill="blue", width=0.5) +
     theme_bw() +
     labs(x="Flu Strain", y="Peptide Counts")}
+
 
